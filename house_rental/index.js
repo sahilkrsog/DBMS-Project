1fcff8ed8043393baeadeb981bdef66384d59063
sahil <sk9711092314@gmail.com>
2021-10-13 14:53:45 +0530

    Add project files.

---
+++ house_rental/index.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("moreInfo").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nihil non saepe error delectusLorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nihil non saepe error delectusLorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nihil non saepe error delectus";
+    
+}
